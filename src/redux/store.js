5a844df0d47e3815d9240531eb4b9e0ae2386fae
bianth <bianth@mvtech.com.cn>
2020-11-23 11:09:14 +0800

    redux使用

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 
-import {createStore} from 'redux'
+import {createStore,applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import {counter} from './reducers'
-const store = createStore(counter)
+const store = createStore(counter,composeWithDevTools(applyMiddleware(thunk)))
 export default store
